e1015f962689a57f0ad0fb5767c7d719048a5ac0
Fahia Mohamed <fahia.mohamed@shopify.com>
2019-03-03 17:18:49 -0500

    New post and blog template

---
+++ _site/assets/js/lunrsearchengine.js
@@ -0,0 +1,94 @@
+
+var documents = [{
+    "id": 0,
+    "url": "http://localhost:4000/404.html",
+    "title": "404",
+    "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "http://localhost:4000/about",
+    "title": "About me",
+    "body": ""
+    }, {
+    "id": 2,
+    "url": "http://localhost:4000/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 3,
+    "url": "http://localhost:4000/",
+    "title": "Home",
+    "body": "            All Stories:                                                                       Why you should consider fasting              :       The conventional western diet prescribes 3 meals per day. This derives from the early European settlers that brought it to North America. They were shocked by the Native Americans eating. . . :                                                                             Fahia                03 Mar 2018                                                       "
+    }, {
+    "id": 4,
+    "url": "http://localhost:4000/fasting/",
+    "title": "Why you should consider fasting",
+    "body": "2018/03/03 - The conventional western diet prescribes 3 meals per day. This derives from the early European settlers that brought it to North America. They were shocked by the Native Americans eating habits, a simple but effective dietary system; eat when you’re hungry. This lack of rigidity was considered uncivilized and settlers introduced the 3 meal per day system. The earliest record of this dates back to middle age Europe. Our history spans thousands of years. The first piece of human art, the Lion-man of the Hohlenstein-Stadel, dates back 30,000 years [1]. Until the Agricultural Revolution (roughly 10,000 BC), eating 3 meals per day was a near impossibility. Food was not readily available because of the lack of preservation. It was not uncommon to eat a few times per week. Humans maintained extended periods without eating as a way of life for thousands of years. What about hunger?: The topic of hunger usually follows directly after discussing the basic premise of fasting. Hunger as we know it is a very complex biochemical reaction that starts in the gut. The hormone, Ghrelin, known affectionately as the “hunger hormone” stimulates appetite, increases food intake and promotes fat storage. A logical conclusion would be that hunger increases linearly over time as you go without food.  As we can see in this chart derived from testing the Ghrelin levels in patients that partook in a 3 day fast [2], hunger comes in waves. Interestingly, the chart indicates that the patients were the least hungry in the morning. Our body learns our eating habits and this aligns with the Ghrelin spikes. We can see hunger decreases after each of these spikes in a rhythm that follows our eating schedule. On average, each patient lost their desire to eat 2 hours after their Ghrelin spike despite not eating. Hunger does not linearly increase over time and is not constant. Dr. Fung, the author of the book Obesity Code, says this about the study:  “Over 3 days of fasting, ghrelin gradually decreased. This means that patients were far less hungry despite not having eaten for the past 3 days. This jives perfectly with our clinical experience with patients undergoing extended fasting. They all expect to be ravenously hungry, but actually find that their hunger completely disappears. ” To quote Dr. Yoshinori Nagumo, who eats one meal a day, “Hunger makes people healthy”. Western civilization has traditionally had a hate/hate relationship with hunger, silencing it the second it arises, avoiding it at all costs. This, as we can see, does more harm than good. Fasting vs Calorie Restriction: Obesity is a hormonal imbalance and not a caloric one. Fasting triggers numerous hormonal adaptations that do not happen with simple caloric reduction. Insulin drops, helping prevent insulin resistance. Noradrenalin rises, keeping metabolism high. Growth hormone rises, maintaining lean mass. When the contestants on The Biggest Loser dropped their caloric intake by half, their basal metabolic rate drastically dropped. This, in the short-term, does lead to weight loss. Unfortuantely, the lowered basal metabolic rate means that weight loss plateaus. Once expenditure drops below intake, the weight comes back. This massive drop in caloric intake puts your body into starvation mode. The key is to maintain your basal metabolism. Fasting increases the basal metabolic rate.  In the chart above, the metabolic rate increased 12% over a period of 4 days of fasting. What are the takeaways here? Fasting provides beneficial hormonal changes while calorie restriction does not. This, however, does not invalidate caloric restriction. Fasting is a strictly more efficient method of fat loss. It is the intermittency of the fasting that makes it so much more effective. Fasting and muscle loss: A common misconception is that you cannot fast and maintain muscle mass. As the length of time without food increases, the basal metabolic rate increases slightly. Lack of incoming energy makes the body change energy sources from incoming energy to stored energy. Also known as body fat. During fasting, the body first burns glycogen stored in the liver. When that is finished, it moves to body fat. Homo Sapiens would have become extinct long ago if our bodies slow down each time we didn’t eat for a few hours. Using stored fat as an energy source meant that our ancestors could go without food for prolonged periods and continue to have enough energy to hunt and provide. It’s counter-intuitive for the human body to go for one of the most important components of our physiology (our muscles) if there was something already present that was easier to break down and use as energy. It’s important to note that the process of breaking down muscle for energy is an infrequent, last resort for the body. Ghrelin binds to the Human Growth Hormone (HGH) secretagogue receptor, which secretes HGH. HGH secretion decreases as food intake increases. Conversely, HGH secretion increases as food intake decreases. Your body is actively preventing muscle loss by increasing HGH secretion! Longevity and fasting: Whether a yeast cell, a mouse or a rhesus monkey, research shows that food restriction over a period of time will almost always increase longevity in animals. They live longer the less they eat [3]. This in and of itself is extraordinary, but it’s worth digging into exactly what sorts of things cause this. In the 1950s, Belgian scientist Christian de Duve was studying insulin when he accidentally discovered a process he called autophagy, from the Greek words for “self” (auto) and “eating” (phagy). It is the mechanism by which cells cannibalize some of their own parts in a continual cleanup process. In 1983, Yoshinori Ohsumi discovered the genes that regulate autophagy (and later in 2016, received a Nobel Prize for the research). Autophagy occurs when there’s cellular stress. If cells lack nutrients, are deprived of energy, or are damaged in some way, a “stress response” mechanism is activated, which initiates autophagy. As a result, cell function actually improves when we’re under cellular stress. In the absence of added stress, autophagy remains functioning at a moderate level, maintaining cell function in maintenance mode. When fasting, you’re invoking autophagy. This triggers your cleanup crew to go in and get rid of the old, dirty cells. The result being cleaner, younger, and healthier cells. Autophagy and its relationship with aging is a hot topic. Aging is the gradual failure over time of cellular repair mechanisms that leads to the accumulation of molecular and cellular damage and loss of function. The cell’s capacity for autophagic degradation also declines with age, and this in itself may contribute to the aging process. Thus, if autophagy maintains healthy cell function, cleans and repurposes old cells, fasting can slowing aging [4][5][6]. After a few days of fasting, it is not uncommon to enter what’s known as ketosis. Ketosis is a physiological state that causes your body to switch over to using fat for energy instead of glucose. This makes ketosis an efficient fat-loss method without sacrificing muscle mass. ketosis also increases mental and physical efficiency. Before the agricultural revolution, humans primarily functioned in ketosis because wheat, grains, and rice were not easily harvested. Our ancestors functioned in ketosis for thousands of years without issue. It was only until we started farming that our bodies jumped from fat to glucose for energy and with that, problems ensued. Ketosis occurs when carbohydrate intake is kept to a minimum while also keeping protein low. The percentages would be 5% carbs, 25% proteins, and the rest fats. A process called Gluconeogenesis will occur if the body cannot use fat for fuel. This process converts muscle into glucose which is usually a last ditch effort because the body doesn’t want to use something as important as muscle tissue for energy if it doesn’t have to. This is why you are more likely to have muscle loss during caloric restriction versus when not eating anything at all. I was motivated to write this post to serve as an aggregation of reasons why I started fasting. It is not meant to be an exhaustive collection of all possible reasons why fasting should be part of your lifestyle. I am hoping that by reading this, you continue to research further and experiment as I have. There will be a series of blog posts after this to discuss other things about food and health. If you’re interesting in learning more, there are a ton of resources out there and folks like Dr. Jason Fung and Dr. Peter Attia have written at length about this topic. [1] http://www. loewenmensch. de/figur_3. html [2] https://www. ncbi. nlm. nih. gov/pubmed/15522942 [3] https://academic. oup. com/ajcn/article/78/3/361/4689958 [4] https://www. ncbi. nlm. nih. gov/pubmed/16884547 [5] https://www. ncbi. nlm. nih. gov/pubmed/18219227 [6] https://www. ncbi. nlm. nih. gov/pubmed/18282106 "
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><small><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
